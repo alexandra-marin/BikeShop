cb48307deb912667357afb2c33088d58a0ed21e9
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-11 23:57:54 +0300

    Remove unused method

--- BikeDistributor/Receipt/ReceiptFormats.cs
+++ BikeDistributor/Receipt/ReceiptFormats.cs
@@ -46,33 +46,5 @@ namespace BikeDistributor
 
             return view.ToString();
         }
-
-        private double CalculateAmount(Line line)
-        {
-            var thisAmount = 0d;
-            switch (line.Bike.Price)
-            {
-                case Bike.OneThousand:
-                    if (line.Quantity >= 20)
-                        thisAmount += line.Quantity * line.Bike.Price * .9d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-                case Bike.TwoThousand:
-                    if (line.Quantity >= 10)
-                        thisAmount += line.Quantity * line.Bike.Price * .8d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-                case Bike.FiveThousand:
-                    if (line.Quantity >= 5)
-                        thisAmount += line.Quantity * line.Bike.Price * .8d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-            }
-
-            return thisAmount;
-        }
     }
 }
