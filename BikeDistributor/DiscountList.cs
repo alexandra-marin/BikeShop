0df5c92e99dbb80b0686c20e039eafd7a2469d14
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-12 00:56:37 +0300

    Introduce discounts repo

---
+++ BikeDistributor/DiscountList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BikeDistributor
+{
+    public static class DiscountList
+    {
+        public static Dictionary<int, DiscountCondition> DiscountFor = new Dictionary<int, DiscountCondition>()
+        {
+            [Bike.OneThousand]  = new DiscountCondition(20, .9d),
+            [Bike.TwoThousand]  = new DiscountCondition(10, .8d),
+            [Bike.FiveThousand] = new DiscountCondition( 5, .8d),
+        };
+    }
+}
--- BikeDistributor/LineAmountCalculator.cs
+++ BikeDistributor/LineAmountCalculator.cs
@@ -14,20 +14,7 @@ namespace BikeDistributor
 		public double CalculateAmount()
 		{
 			var thisAmount = 0d;
-            var discount = new DiscountCondition(0, 1d);
-
-            switch (line.Bike.Price)
-            {
-                case Bike.OneThousand:
-                    discount = new DiscountCondition(20, .9d);
-                    break;
-                case Bike.TwoThousand:
-                    discount = new DiscountCondition(10, .8d);
-                    break;
-                case Bike.FiveThousand:
-                    discount = new DiscountCondition(5, .8d);
-                    break;
-            }
+            var discount = DiscountList.DiscountFor[line.Bike.Price];
 
             if (line.Quantity >= discount.MinEligibleQuantity)
                 thisAmount += line.Quantity * line.Bike.Price * discount.DiscountedBy;
