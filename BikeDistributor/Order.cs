2bfe0e4420cb5d56cff714591056295f6ac31c63
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-11 20:13:27 +0300

    Extract receipt class from Order

--- BikeDistributor/Order.cs
+++ BikeDistributor/Order.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace BikeDistributor
 {
     public class Order
     {
-        private const double TaxRate = .0725d;
-		private readonly IList<Line> _lines = new List<Line>();
+        public string Company { get; private set; }
+       
+        private readonly IList<Line> _lines = new List<Line>();
+        private readonly ReceiptBuilder receiptBuilder = new ReceiptBuilder();
 
         public Order(string company)
         {
             Company = company;
         }
-
-        public string Company { get; private set; }
 
         public void AddLine(Line line)
         {
@@ -24,70 +21,12 @@ namespace BikeDistributor
 
         public string Receipt()
         {
-            var totalAmount = 0d;
-			var result = new StringBuilder(string.Format("Order Receipt for {0}{1}", Company, Environment.NewLine));
-            foreach (var line in _lines)
-			{
-                double thisAmount = CalculateAmount(line);
-                result.AppendLine(string.Format("\t{0} x {1} {2} = {3}", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
-				totalAmount += thisAmount;
-			}
-			result.AppendLine(string.Format("Sub-Total: {0}", totalAmount.ToString("C")));
-            var tax = totalAmount * TaxRate;
-            result.AppendLine(string.Format("Tax: {0}", tax.ToString("C")));
-            result.Append(string.Format("Total: {0}", (totalAmount + tax).ToString("C")));
-            return result.ToString();
+            return receiptBuilder.Receipt(Company, _lines);
         }
 
-		public string HtmlReceipt()
+        public string HtmlReceipt()
         {
-            var totalAmount = 0d;
-            var result = new StringBuilder(string.Format("<html><body><h1>Order Receipt for {0}</h1>", Company));
-            if (_lines.Any())
-            {
-                result.Append("<ul>");
-                foreach (var line in _lines)
-                {
-                    double thisAmount = CalculateAmount(line);
-                    result.Append(string.Format("<li>{0} x {1} {2} = {3}</li>", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
-                    totalAmount += thisAmount;
-                }
-                result.Append("</ul>");
-            }
-            result.Append(string.Format("<h3>Sub-Total: {0}</h3>", totalAmount.ToString("C")));
-            var tax = totalAmount * TaxRate;
-            result.Append(string.Format("<h3>Tax: {0}</h3>", tax.ToString("C")));
-            result.Append(string.Format("<h2>Total: {0}</h2>", (totalAmount + tax).ToString("C")));
-            result.Append("</body></html>");
-            return result.ToString();
-        }
-
-        private double CalculateAmount(Line line)
-        {
-            var thisAmount = 0d;
-            switch (line.Bike.Price)
-            {
-                case Bike.OneThousand:
-                    if (line.Quantity >= 20)
-                        thisAmount += line.Quantity * line.Bike.Price * .9d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-                case Bike.TwoThousand:
-                    if (line.Quantity >= 10)
-                        thisAmount += line.Quantity * line.Bike.Price * .8d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-                case Bike.FiveThousand:
-                    if (line.Quantity >= 5)
-                        thisAmount += line.Quantity * line.Bike.Price * .8d;
-                    else
-                        thisAmount += line.Quantity * line.Bike.Price;
-                    break;
-            }
-
-            return thisAmount;
+            return receiptBuilder.HtmlReceipt(Company, _lines);
         }
     }
 }
---
+++ BikeDistributor/ReceiptBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BikeDistributor
+{
+    public class ReceiptBuilder
+    {
+        private const double TaxRate = .0725d;
+       
+        public string Receipt(string Company, IList<Line> _lines)
+        {
+            var totalAmount = 0d;
+            var result = new StringBuilder(string.Format("Order Receipt for {0}{1}", Company, Environment.NewLine));
+            foreach (var line in _lines)
+            {
+                double thisAmount = CalculateAmount(line);
+                result.AppendLine(string.Format("\t{0} x {1} {2} = {3}", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
+                totalAmount += thisAmount;
+            }
+            result.AppendLine(string.Format("Sub-Total: {0}", totalAmount.ToString("C")));
+            var tax = totalAmount * TaxRate;
+            result.AppendLine(string.Format("Tax: {0}", tax.ToString("C")));
+            result.Append(string.Format("Total: {0}", (totalAmount + tax).ToString("C")));
+            return result.ToString();
+        }
+
+        public string HtmlReceipt(string Company, IList<Line> _lines)
+        {
+            var totalAmount = 0d;
+            var result = new StringBuilder(string.Format("<html><body><h1>Order Receipt for {0}</h1>", Company));
+            if (_lines.Any())
+            {
+                result.Append("<ul>");
+                foreach (var line in _lines)
+                {
+                    double thisAmount = CalculateAmount(line);
+                    result.Append(string.Format("<li>{0} x {1} {2} = {3}</li>", line.Quantity, line.Bike.Brand, line.Bike.Model, thisAmount.ToString("C")));
+                    totalAmount += thisAmount;
+                }
+                result.Append("</ul>");
+            }
+            result.Append(string.Format("<h3>Sub-Total: {0}</h3>", totalAmount.ToString("C")));
+            var tax = totalAmount * TaxRate;
+            result.Append(string.Format("<h3>Tax: {0}</h3>", tax.ToString("C")));
+            result.Append(string.Format("<h2>Total: {0}</h2>", (totalAmount + tax).ToString("C")));
+            result.Append("</body></html>");
+            return result.ToString();
+        }
+
+        private double CalculateAmount(Line line)
+        {
+            var thisAmount = 0d;
+            switch (line.Bike.Price)
+            {
+                case Bike.OneThousand:
+                    if (line.Quantity >= 20)
+                        thisAmount += line.Quantity * line.Bike.Price * .9d;
+                    else
+                        thisAmount += line.Quantity * line.Bike.Price;
+                    break;
+                case Bike.TwoThousand:
+                    if (line.Quantity >= 10)
+                        thisAmount += line.Quantity * line.Bike.Price * .8d;
+                    else
+                        thisAmount += line.Quantity * line.Bike.Price;
+                    break;
+                case Bike.FiveThousand:
+                    if (line.Quantity >= 5)
+                        thisAmount += line.Quantity * line.Bike.Price * .8d;
+                    else
+                        thisAmount += line.Quantity * line.Bike.Price;
+                    break;
+            }
+
+            return thisAmount;
+        }
+    }
+}
