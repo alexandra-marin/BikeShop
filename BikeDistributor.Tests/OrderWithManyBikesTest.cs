ba937cc057d306bc48a1b33de98b2d84a4761c89
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-25 20:45:10 +0300

    Tests namespace

--- BikeDistributor.Tests/OrderWithManyBikesTest.cs
+++ BikeDistributor.Tests/OrderWithManyBikesTest.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 
-namespace BikeDistributor.Test
+namespace BikeDistributor
 {
     [TestFixture]
     public class OrderWithManyBikesTest
--- BikeDistributor.Tests/OrderWithManyLinesTest.cs
+++ BikeDistributor.Tests/OrderWithManyLinesTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 
-namespace BikeDistributor.Tests
+namespace BikeDistributor
 {
 	[TestFixture]
 	public class OrderWithManyLinesTest
