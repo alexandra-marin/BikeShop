f5da7913b9c41deaac35becd7ce56c4a0f3264d9
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-25 21:55:57 +0300

    Cleanup

--- BikeDistributor/Line.cs
+++ BikeDistributor/Line.cs
@@ -6,12 +6,12 @@
         public int    Quantity { get; private set; }
         public double Amount   { get; private set; }
 
-        public Line(Bike bike, int quantity, Discounts discountedPrice)
+        public Line(Bike bike, int quantity, Discounts discount)
         {
             Bike = bike;
             Quantity = quantity;
 
-            var calculator = new LineAmount(bike.Price, quantity, discountedPrice);
+            var calculator = new LineAmount(bike.Price, quantity, discount);
             Amount = calculator.Calculate();
         }
     }
