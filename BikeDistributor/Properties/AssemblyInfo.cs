10f5ec5f09837c8980bca33f045117452f0d4a3d
Alexandra Marin <alexandra.marin01@gmail.com>
2017-04-25 21:16:03 +0300

    Fix assembly info

--- BikeDistributor/Properties/AssemblyInfo.cs
+++ BikeDistributor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MovieStore")]
+[assembly: AssemblyTitle("BikeDistributor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MovieStore")]
+[assembly: AssemblyProduct("BikeDistributor")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
